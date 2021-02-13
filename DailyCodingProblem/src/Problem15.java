2a7d63446a76bd16381f42d56ab9f45faa63a7cd
ardatiftikci <ardatiftikci@gmail.com>
2021-02-13 20:38:22 +0300

    Problem 15 is added.

---
+++ DailyCodingProblem/src/Problem15.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+/* Given a stream of elements too large to store in memory, pick a random element from the stream with uniform probability.
+ */
+
+public class Problem15 {
+	private static final Random rand = new Random();
+	public static void main(String[] args) {
+		p15();
+	}
+
+	public static void p15() {
+		int[] bigStream = {1,2,3,4,5,6,7,8};
+		int count = 1;
+		int randomSelectedNumber=bigStream[0];
+		for(int i=1; i< bigStream.length; i++) {
+			count++;
+			randomSelectedNumber = randomSelect(bigStream[i], randomSelectedNumber, count);
+		}
+		
+		System.out.println("Randomly selected number is "+randomSelectedNumber);
+	}
+
+	private static int randomSelect(int currentStreamElement, int currentSelectedNumber, int count) {
+		if (rand.nextInt(count)==0) return currentStreamElement;
+		else return currentSelectedNumber;
+	}
+
+}
